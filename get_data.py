5593698d143a6c06c19c809ab2b0d5b5f259ccb1
Ramble21 <zramble21@gmail.com>
2025-08-15 14:28:54 -0400

    get_data function

---
+++ get_data.py
@@ -0,0 +1,28 @@
+import chess.pgn
+
+def get_data(num_games, log_freq):
+    games = []
+    with open("games.pgn") as pgn:
+        i = 0
+        while True:
+            game = chess.pgn.read_game(pgn)
+            has_eval = False
+            for node in game.mainline():
+                comment = node.comment
+                if "[%eval" in comment:
+                    has_eval = True
+                    break
+            if has_eval:
+                games.append(game)
+                i += 1
+                if i % log_freq == 0:
+                    print(f"{i} games processed")
+            if game is None or i == num_games:
+                break
+
+    with open("games_f.pgn", "w", encoding="utf-8") as out_pgn:
+        for g in games:
+            exporter = chess.pgn.FileExporter(out_pgn)
+            g.accept(exporter)
+
+get_data(num_games=20, log_freq=10)
